b4c22c41851ad1b54239e0abb6f04bbda264eb94
Amanda Tange <amandatange@hotmail.com>
2022-03-29 13:36:17 +0100

    added background image component and moved styling for header to stylesheet component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Stock from './components/Stock';
 import warehouse from './assets/warehouse.jpg';
+import plant from './assets/plant.jpg';
 
 
 export default function App() {
@@ -12,24 +13,33 @@ export default function App() {
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<View style={styles.base}>
-				<Text style={{color: '#33c', fontSize: 42}}>Lager-Appen</Text>
-				<Image source={warehouse} style={{ width: 320, height: 240 }} />
-				<Stock />
-				<StatusBar style="auto" />
-			</View>
+			<ImageBackground source={plant} resizeMode="cover" style={styles.background}>
+				<View style={styles.base}>
+					<Text style={styles.header}>PlantHome</Text>
+					{/* <Image source={warehouse} style={{ width: 320, height: 240 }} /> */}
+					<Stock />
+					<StatusBar style="auto" />
+				</View>
+			</ImageBackground>
 		</SafeAreaView>
 		);
-	}
+}
 
-	const styles = StyleSheet.create({
-		container: {
+const styles = StyleSheet.create({
+	header: {
+		color: 'hsl(120, 15%, 50%)',
+		fontSize: 42,
+		textAlign: "center"
+	},
+	container: {
 		flex: 1,
-		},
-		base: {
+	},
+	background: {
+		flex: 1
+	},
+	base: {
 		flex: 1,
-		backgroundColor: '#fff',
-		paddingLeft: 12,
-		paddingRight: 12,
-		}
-	});
+		paddingLeft: 30,
+		paddingRight: 30,
+	}
+});
